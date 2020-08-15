64b7e834152b934538b6c67f78fd9e68c8100a83
Omar <omar.caja@hotmail.com>
2020-08-15 23:22:50 +0200

    Added all solutions methods in bactracking interface

--- src/backtracking/backtracking.py
+++ src/backtracking/backtracking.py
@@ -17,3 +17,11 @@ class Backtracking(abc.ABC):
     @abc.abstractmethod
     def solve(self):
         pass
+
+    @abc.abstractmethod
+    def _backtracking_all_solutions(self, *args):
+        pass
+
+    @abc.abstractmethod
+    def solve_all_solutions(self):
+        pass
